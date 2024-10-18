2a58c81b0b8cf37fcdebd51e6772200b83f3779e
prerakshah <prerakshah@dell15>
2024-10-18 23:12:31 +0530

    Created Test User class for Second Commit.

---
+++ src/main/java/com/creditWise/DataHandler/UserConstant.java
@@ -0,0 +1,49 @@
+package com.creditWise.DataHandler;
+
+public class UserConstant
+{
+	String	userName;
+	String	userPassword;
+	String	userEmail;
+	String	userContact;
+
+	public String getUserName()
+	{
+		return userName;
+	}
+
+	public void setUserName(String userName)
+	{
+		this.userName = userName;
+	}
+
+	public String getUserPassword()
+	{
+		return userPassword;
+	}
+
+	public void setUserPassword(String userPassword)
+	{
+		this.userPassword = userPassword;
+	}
+
+	public String getUserEmail()
+	{
+		return userEmail;
+	}
+
+	public void setUserEmail(String userEmail)
+	{
+		this.userEmail = userEmail;
+	}
+
+	public String getUserContact()
+	{
+		return userContact;
+	}
+
+	public void setUserContact(String userContact)
+	{
+		this.userContact = userContact;
+	}
+}
