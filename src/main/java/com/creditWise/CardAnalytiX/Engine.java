82c2bbdcb3114ae5bfcba2f5d57885947eb84d13
prerakshah <prerakshah@dell15>
2024-12-01 10:02:14 +0530

    Made generalized Engine class

--- src/main/java/com/creditWise/CardAnalytiX/Engine.java
+++ src/main/java/com/creditWise/CardAnalytiX/Engine.java
@@ -3,44 +3,46 @@ package com.creditWise.CardAnalytiX;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.reflect.Array;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Engine
 {
 	public static ArrayList<CreditCard> Engine1() throws IOException
 	{
+		HashMap<String, String> bankToPath = new HashMap<String, String>();
 
 		String SoctiaBankData = "/home/prerakshah/git/CardAnalytiX/text_pages/scotiabank_cards.txt";
-		String TDBankData = "/home/prerakshah/git/CardAnalytiX/text_pages/scotiabank_cards.txt";
-		String RBCBankData = "/home/prerakshah/git/CardAnalytiX/text_pages/scotiabank_cards.txt";
-		String CIBCBankData = "/home/prerakshah/git/CardAnalytiX/text_pages/scotiabank_cards.txt";
-		String BankData[] = {SoctiaBankData, TDBankData};
+		String TDBankData = "/home/prerakshah/git/CardAnalytiX/text_pages/td_cards.txt";
+		String RBCBankData = "/home/prerakshah/git/CardAnalytiX/src/main/resources/rbc_cards.txt";
+		String CIBCBankData = "/CardAnalytiX/src/main/resources/cibc_cards.txt";
+		bankToPath.put("TD Bank", TDBankData);
+		bankToPath.put("Soctia Bank", SoctiaBankData);
+		//		bankToPath.put("CIBC", CIBCBankData);
+		//		bankToPath.put("RBC", RBCBankData);
 
-		//		CardList has all the creditcard data. Use this list for data.
-		ArrayList<CreditCard> cardList = cardObjectFiller(BankData);
-		//		for(CreditCard creditCard : cardList)
-		//		{
-		//			System.out.println(creditCard.getCardType());
-		//		}
+		ArrayList<CreditCard> cardList = cardObjectFiller(bankToPath);
 		return cardList;
 	}
 
-	private static ArrayList<CreditCard> cardObjectFiller(String[] BankDataPath) throws IOException
+	private static ArrayList<CreditCard> cardObjectFiller(HashMap<String, String> BankDataPath) throws IOException
 	{
 		ArrayList<CreditCard> cardList = new ArrayList<CreditCard>();
 
-		for(String bankPath : BankDataPath)
+		for(Map.Entry<String, String> set : BankDataPath.entrySet())
 		{
-			BufferedReader br = new BufferedReader(new FileReader(bankPath));
+			//			InputStream inputStream = Engine.class.getClassLoader().getResourceAsStream(set.getValue());
+			BufferedReader br = new BufferedReader(new FileReader(set.getValue()));
 			String line = "";
-			String bankName = bankPath.split("/")[6].split("_")[0];
+			//			String bankName = bankPath.split("/")[6].split("_")[0];
 			while((line = br.readLine()) != null)
 			{
 				String[] tsvElement = line.split("\t");
-				cardList.add(new CreditCard(tsvElement[0], tsvElement[1], tsvElement[2], tsvElement[3], tsvElement[4], bankName));
+				cardList.add(new CreditCard(tsvElement[0], tsvElement[1], tsvElement[2], tsvElement[3], tsvElement[4], set.getKey()));
 			}
-
 		}
 
 		return cardList;
